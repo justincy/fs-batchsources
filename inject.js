54bff180495fd18bbab88c283d0be3be9d450452
Justin York <justincyork@gmail.com>
2012-11-13 19:55:33 -0700

    Rearrange files; add css file

---
+++ inject.js
@@ -0,0 +1,7 @@
+(function(){
+
+  var scriptTag = document.createElement('script');
+  scriptTag.src = 'https://dl.dropbox.com/u/23443250/batch-sources.js';
+  document.body.appendChild(scriptTag);
+
+}());
--- js/inject.js
+++
@@ -1,7 +0,0 @@
-(function(){
-
-  var scriptTag = document.createElement('script');
-  scriptTag.src = 'https://dl.dropbox.com/u/23443250/batch-sources.js';
-  document.body.appendChild(scriptTag);
-
-}());
