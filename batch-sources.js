a4dca60271436719ebf56214cbc61ca5adcd9f9d
Justin York <justincyork@gmail.com>
2012-11-13 20:59:11 -0700

    Add the batch-sources.js file
    Dropbox throttles :(

---
+++ batch-sources.js
@@ -0,0 +1,354 @@
+$(function(){
+
+  // Regex used for matching personIds
+  var personIdReg = /[0-9A-Za-z]{4}\-[0-9A-Za-z]{3}/;
+  
+  var SOURCE_STATUS_NA = 0;
+  var SOURCE_STATUS_SUCCESS = 1;
+  var SOURCE_STATUS_FAIL = 2;
+  
+  // Global object used for collecting statuses
+  // It is keyed by the index of the source object
+  // in the sources array. It is cleared out every
+  // time the save process is started.
+  var sourceStatuses = {};
+
+  // Get the FS configuration from the page
+  var config = JSON.parse($('aside').attr('data-config'));
+  
+  // Scrape the page for information about the main person in this record
+  var personData = getPersonData();
+
+  // Gather all links, starting with the current page
+  var sourceLinks = [ 
+    { 
+      name: personData.givenName + ' ' + personData.familyName, 
+      url: document.location.href 
+    } 
+  ];
+  $('table.result-data a').each(function(){
+    
+    var link = $(this).attr('href');
+    
+    // Make sure the link points to a record
+    if( link.indexOf('https://familysearch.org/pal:/') == 0 ) {
+      sourceLinks.push({ name: $(this).text(), url: link });
+    }
+    
+  });
+
+  // If there is more than one link, switch to batch mode
+  if( sourceLinks.length > 1 && config.userSignedIn ) {
+  
+    // Create confirmation dialog
+    var confirmationDialogInner = $('<div>').addClass('modal-inner clearfix');
+    $('<h2>').html('Sources Added to My Source Box').appendTo(confirmationDialogInner);
+    $('<div id="batch-sources-messages">').appendTo(confirmationDialogInner);
+    $('<div id="batch-source-dialog-confirmation">').addClass('message-dialog')
+      .append(confirmationDialogInner).wrap('<div style="display:none;">').parent().appendTo('#main');
+    
+    // Create batch add dialog
+    var dialogInner = $('<div>').addClass('modal-inner clearfix');
+    $('<h2>').html('Which sources do you want to add?').appendTo(dialogInner);
+    
+    // Setup the source selection list
+    var sourceList = $('<table>').addClass('batch-source-list').appendTo(dialogInner);
+    $.each(sourceLinks, function(i, s){
+      var row = $('<tr>').appendTo(sourceList);
+      
+      // Create cell that contains checkbox and person name
+      $('<label>').append( $('<input type="checkbox" value="' + i + '" CHECKED >').addClass('batch-source-checkbox') )
+        .append( $('<span>').html( s.name ) )
+        .appendTo( $('<td>').appendTo(row) );
+        
+      // Create cell that contains input field for person ID
+      $('<td>').appendTo(row).append( $('<input id="source-attach-' + i + '" type="text">').addClass('batch-source-person-id') );
+    });
+    
+    // Add toggle-all links
+    $('<a href="#">All</a>').css('margin-right', '10px').click(function(){
+      $('.batch-source-checkbox').attr('checked', true);
+    }).appendTo(dialogInner);
+    $('<a href="#">None</a>').css('margin-right', '10px').click(function(){
+      $('.batch-source-checkbox').attr('checked', false);
+    }).appendTo(dialogInner);
+    
+    // Create add button and click event handler
+    $('<button>').addClass('form-submit').html('Add Sources').css('float', 'right').click(function(){
+      
+      // Gather list of sources to add
+      var requestedSources = [];
+      $('.batch-source-checkbox:checked').each(function(){
+        // The value of the checkboxes is the index of the
+        // object in the soruceLinks array
+        var sourceIndex = $(this).val();
+        var source = sourceLinks[sourceIndex];
+        
+        // Add a personId if the user requested that the
+        // source also be attached to a person
+        var personId = $('#source-attach-' + sourceIndex).val().toUpperCase();
+        if( personIdReg.test( personId ) ) {
+          source.personId = personId;
+        }
+        
+        requestedSources.push( source );
+      });
+      
+      // Reset the source statuses
+      sourceStatuses = {};
+      
+      // Save the sources
+      batchSourceSave(requestedSources).always(function(){
+        
+        // Process the source statuses and create the relavent messages
+        var messageList = $('#batch-sources-messages').html('');
+        $.each(requestedSources, function(i, source){
+          var message;
+          if( source.create === SOURCE_STATUS_SUCCESS) {
+            if( source.attach === SOURCE_STATUS_NA ) {
+              message = 'A source was created for ' + source.name + '.';
+            } else if( source.attach === SOURCE_STATUS_SUCCESS ) {
+              message = 'A source was created and attached for ' + source.name + '.';
+            } else if( source.attach === SOURCE_STATUS_FAIL ) {
+              message = 'A source was created for ' + source.name + ' but we failed to attach it.';
+            }
+          } else {
+            message = 'We failed to created the source for ' + source.name + '.';
+          }
+          
+          messageList.append( $('<div>').addClass('batch-source-message').html(message) );
+        });
+        
+        $.fancybox({ href: '#batch-confirmation-dialog-confirmation',  padding: 0, overlayColor: '#fff' });
+        
+      });
+      
+    }).appendTo(dialogInner);
+    
+    // Add the batch dialog to the page
+    $('<div id="batch-source-dialog">').addClass('message-dialog')
+      .append(dialogInner).wrap('<div style="display:none;">').parent().appendTo('#main');
+    
+    // Add the batch add button to the toolbar
+    $('.flyout .source-add').after(
+      $('<li>').append(
+        $('<a href="#">BATCH SOURCE ADD</a>').click(function(){
+          $.fancybox({ href: '#batch-source-dialog',  padding: 0, overlayColor: '#fff' });
+        })
+      )
+    );
+  
+  }
+  
+  function batchSourceSave(sources) {
+    
+    var deferreds = [];
+    
+    $.each(sources, function(i, source){
+      
+      var createDeferred = $.ajax({
+        type: 'POST',
+        url: '/links/source?sessionId=' + config.sessionId,
+        dataType: 'json',
+        data: '{ "uri" : { "uri" : "' +  source.url + '" }, "sourceType" : "FSREADONLY"}',
+        processData: false,
+        contentType: "application/json",
+        accept: 'application/json; charset=utf8'
+      }).done(function(){
+        source.create = SOURCE_STATUS_SUCCESS;
+      }).fail(function(){
+        source.create = SOURCE_STATUS_FAIL;
+      });
+      
+      // If the source should be attached to a person,
+      // add the success handler which will do that
+      if( source.personId ) {
+        createDeferred.done(function(json){
+          deferreds.push( 
+            attachSource(json.id, source.personId).done(function(){
+              source.attach = SOURCE_STATUS_SUCCESS;
+            }).fail(function(){
+              source.attach = SOURCE_STATUS_FAIL;
+            })
+          );
+        });
+      } else {
+        source.attach = SOURCE_STATUS_NA;
+      }
+      
+      deferreds.push( createDeferred );
+      
+    });
+    
+    return $.when.apply(deferreds);
+    
+  };
+  
+  function attachSource(sourceId, personId) {
+    return $.ajax({
+      type: 'POST',
+      url: '/ct/persons/' + personId + '/source-references/source-reference?mediaType=application/json',
+      dataType: 'json',
+      data: '{"id":null, "conclusionTypes":[], "contribution":{"timeStamp":null, "contributorId":null, "submitterId":null}, "entityId":"' + personId + '", "justification":{"reason":"", "confidence":null}, "sourceReferenceType":null, "sourceUri":"' + sourceId + '"}',
+      processData: false,
+      contentType: "application/json",
+      accept: 'application/json; charset=utf8'
+    });
+  };
+
+});
+
+function getPersonData(){
+  
+  var hasFamilyTable = $('.result-data .household-label').length > 0;
+    
+  // Process the table rows of the record data
+  // The first cell in each row becomes the key
+  // The second cell becomes a list of the cells in that row
+  // We store all the cells because the gender of
+  // family members on census records is important
+  var recordData = {};
+  $('.result-data tr').each(function(){
+    var row = $(this);
+    var fieldName = $.trim( $('td:first', row).text().toLowerCase().slice(0, -1) );
+    if( fieldName ) {
+      recordData[fieldName] = $('td', row);
+    }
+  });
+  
+  var nameParts = ['',''];
+  if( recordData['first name'] ) {
+    nameParts = [ getCleanCellValue( recordData['first name'], 1 ) , getCleanCellValue( recordData['last name'], 1 ) ];
+  } 
+  else if(recordData['name']) {
+    nameParts = splitName( getCleanCellValue( recordData['name'], 1 ) );
+  }
+  
+  var personData = {
+    'givenName': nameParts[0],
+    'familyName': nameParts[1],
+    'birthDate': checkMultipleFields( recordData, ['birth date', 'birthdate', 'estimated birth year', 'estimated birth date', 'baptism/christening date'], 1 ),
+    'birthPlace': checkMultipleFields( recordData, ['birthplace', 'place of birth'], 1 ),
+    'deathDate': getCleanCellValue( recordData['death date'], 1 ),
+    'deathPlace': getCleanCellValue( recordData['death place'], 1 )
+  };
+  
+  // Look for a spouse
+  var spouseName = getSpousesName(recordData);
+  if( spouseName ) {
+    var spouseNameParts = splitName( spouseName );
+    personData['spouseGivenName'] = spouseNameParts[0];
+    personData['spouseFamilyName'] = spouseNameParts[1];
+  }
+  
+  // Look for a mother
+  var motherName = getParentName(recordData, 'mother');
+  if( motherName ) {
+    var motherNameParts = splitName( motherName );
+    personData['motherGivenName'] = motherNameParts[0];
+    personData['motherFamilyName'] = motherNameParts[1];
+  }
+
+  // Look for a father
+  var fatherName = getParentName(recordData, 'father');
+  if( fatherName ) {
+    var fatherNameParts = splitName( fatherName );
+    personData['fatherGivenName'] = fatherNameParts[0];
+    personData['fatherFamilyName'] = motherNameParts[1];
+  }
+  
+  return personData;
+  
+  // Check for the existence of multiple fields
+  // First one found is returned
+  function checkMultipleFields( recordData, fields, position ) {
+    for( var i in fields ) {
+      if( recordData[fields[i]] ) {
+        var val = getCleanCellValue( recordData[fields[i]], position );
+        if( val ) {
+          return val;
+        }
+      }
+    }
+    return undefined;
+  };
+  
+  function getCleanCellValue( cells, position ) {
+    if( cells ) {
+      return $.trim( cells.eq(position).text() );
+    }
+    return undefined;
+  };
+  
+  function getRelationship(recordData) {
+    return checkMultipleFields( recordData, ["relationship to head of household", "relationship to head of household (standardized)"], 1 ).toLowerCase();
+  };
+  
+  function getSpousesName(recordData) {
+    // Check to see if the "spouse's name" is set
+    if( recordData["spouse's name"] ) {
+      return getCleanCellValue( recordData["spouse's name"], 1 );
+    }
+    
+    // If "spouse's name" isn't set do some crazy relationship jiu-jitsu
+    else if( hasFamilyTable ) {
+      var relationship = getRelationship(recordData);
+      
+      // The husband is always listed as the head of household
+      // so we only look for the wife. If the wife is the head
+      // of household it means the husband isn't there so returning
+      // the wife will be undefined which means there is no spouse
+      if( relationship == "head" || relationship == "self" ) {
+        return getCleanCellValue( recordData['wife'], 1 );
+      } else if( relationship == "wife" ) {
+        return checkMultipleFields( recordData, ['head', 'self'], 1 );
+      }
+    }
+    
+    return undefined;
+  };
+
+  function getParentName(recordData, parent) {
+    // Check to see if the "parent's name" is set
+    if( recordData[parent+"'s name"] ) {
+      return getCleanCellValue( recordData[parent+"'s name"], 1);
+    }
+    
+    // If "parent's name" isn't set do some crazy relationship jiu-jitsu
+    else if( hasFamilyTable ) {
+      var relationship = getRelationship(recordData);
+      
+      if( relationship == 'son' || relationship == 'daughter' ) {
+        var headGender = checkMultipleFields( recordData, ['head', 'self'], 2 );
+        
+        if( parent == 'father' ) {
+          
+          // Check to see if the gender of the head of household is male
+          if( headGender == 'M' ) {
+            return checkMultipleFields( recordData, ['head', 'self'], 1 );
+          }
+        } else if( parent == 'mother' ) {
+          
+          // If the head of household is male, return the wife's name
+          // If the head of household is female, return the head's name
+          if( headGender == 'F' ) {
+            return checkMultipleFields( recordData, ['head', 'self'], 1 );
+          } else if( headGender == 'M' ) {
+            return getCleanCellValue( recordData['wife'], 1);
+          }
+        }
+      }
+    }
+    
+    return undefined;
+  };
+  
+  function splitName(name) {
+    if( name ) {    
+      return name.split(/\s+(?=\S*$)/);
+    } else {
+      return ['',''];
+    }
+  };
+
+};
